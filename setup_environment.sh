e3963ee59f55015d0f9fd941ea5d9df7314ba0db
MatthewWalstra <you@example.com>
2020-07-08 20:32:44 -0400

    removed WPI/FRC from: setup_environment.sh ros_control_boilerplate CMakeLists.txt removed allWPILib from .gitmodules

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,8 +2,6 @@
 # Script to setup Jetson TX2 environment. Probably would also work
 # with slight modifications on other Jetson hardware
 
-#install basic dependencies
-#sudo apt-add-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt update
 sudo apt -y upgrade
 
@@ -76,279 +74,23 @@ sudo apt -y upgrade
 #sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 30 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 #sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 90 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 
-#install caffe
-# cd
-# git clone https://github.com/BVLC/caffe.git
-# cd caffe
-# mkdir build
-# cd build
-
-# if [ "$gpu" == "false" ] ; then
-	# cmake -DCPU_ONLY=ON ..
-# else
-	# cmake -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF ..
-# fi
-
-# make -j4 all
-#make test
-#make runtest
-# make -j4 install
-
-# Install tinyxml2
-#cd
-#git clone https://github.com/leethomason/tinyxml2.git
-#cd tinyxml2
-#mkdir build
-#cd build
-#cmake -GNinja ..
-#sudo ninja install
-#cd ../..
-#sudo rm -rf tinyxml2
-
-#install zed sdk
-#zed_fn="jetson_jp42"
-#wget --no-check-certificate https://download.stereolabs.com/zedsdk/2.8/$zed_fn
-#chmod 755 $zed_fn
-#./$zed_fn
-#rm ./$zed_fn
-
-#mount and setup autostart script
-#sudo mkdir /mnt/900_2
-#cd ~/2019Offseason
-
-# edit /etc/init.d/ntp to contain the line: <ntpd -gq> before all content already there.
-#sudo cp ntp-client.conf /etc/ntp.conf  # edit /etc/ntp.conf to be a copy of ntp-client.conf in 2019Offseason
-
-# Set up can0 network interface
-#cd
-#echo "auto can0" > can0
-#echo "iface can0 inet manual" >> can0
-#echo "  pre-up /sbin/ip link set can0 type can bitrate 1000000" >> can0
-#echo "  up /sbin/ifconfig can0 up" >> can0
-#echo "  down /sbin/ifconfig can0 down" >> can0
-#sudo mv can0 /etc/network/interfaces.d
-
-#sudo bash -c "echo \"# Modules for CAN interface\" >> /etc/modules"
-#sudo bash -c "echo can >> /etc/modules"
-#sudo bash -c "echo can_raw >> /etc/modules"
-#sudo bash -c "echo can_dev >> /etc/modules"
-#sudo bash -c "echo gs_can >> /etc/modules"
-#sudo bash -c "echo mttcan >> /etc/modules"
-
-# This shouldn't be the least bit dangerous
-#sudo rm /etc/modprobe.d/blacklist-mttcan.conf 
-
-# Disable l4tbridge - https://devtalk.nvidia.com/default/topic/1042511/is-it-safe-to-remove-l4tbr0-bridge-network-on-jetson-xavier-/
-# sudo rm /etc/systemd/system/nv-l4t-usb-device-mode.sh /etc/systemd/system/multi-user.target.wants/nv-l4t-usb-device-mode.service
-#sudo systemctl disable nv-l4t-usb-device-mode.service
-#sudo systemctl stop nv-l4t-usb-device-mode.service
-
-# Set up ssh host config (add port 5801) 
-#sudo sed "s/#Port 22/Port 22\nPort 5801/g" /etc/ssh/sshd_config > sshd_config && sudo mv sshd_config /etc/ssh
-	
-# and keys for connections to Rio
-#mkdir -p ~/.ssh
-#cd ~/.ssh
-#tar -xjf ~/2019Offseason/jetson_setup/jetson_dot_ssh.tar.bz2 
-#chmod 640 authorized_keys
-#cd ~
-#chmod 700 .ssh
-
-#sudo mkdir -p /root/.ssh
-#sudo tar -xjf /home/ubuntu/2019Offseason/jetson_setup/jetson_dot_ssh.tar.bz2 -C /root/.ssh
-#sudo chmod 640 /root/.ssh/authorized_keys
-#sudo chmod 700 /root/.ssh
-
-#cd ~/2019Offseason
-#sudo cp ./jetson_setup/10-local.rules /etc/udev/rules.d/
-#sudo service udev reload
-#sleep 2
-#sudo service udev restart
-
-#if /bin/false; then
-	# Kernel module build steps for TX2 : https://gist.github.com/sauhaardac/9d7a82c23e4b283a1e79009903095655
-	# Not needed unless Jetpack is updated with a new kernel version and modules
-	# for a given kernel version aren't already built
-	#
-	# This is a PR into the jetsonhacks repo of the same name, will likely be
-	# gone the next time anyone looks for it
-	# Also, this says Xavier but it really seems to mean Jetpack 4.x, in the
-	# case of this particular PR, 4.2. Which is what we're using for the TX2
-	# systems as well.
-#	cd
-#	git clone https://github.com/klapstoelpiloot/buildLibrealsense2Xavier.git 
-	# Note - need to switch back to the default linker to build the kernel image
-#	cd
-	#wget https://developer.nvidia.com/embedded/r32-2-3_Release_v1.0/Sources/T186/public_sources.tbz2 
-#	wget https://developer.nvidia.com/embedded/dlc/r32-3-1_Release_v1.0/Sources/T186/public_sources.tbz2
-#	tar -xf public_sources.tbz2 Linux_for_Tegra/source/public/kernel_src.tbz2
-#	mkdir jetson_kernel
-#	cd jetson_kernel
-#	tar -xf ../Linux_for_Tegra/source/public/kernel_src.tbz2
-#	patch -p0 < ~/2019Offseason/patch_j120_l4t32.2.3.txt
-
-	## Apply realsense patches to modules
-#	cd ~/jetson_kernel/kernel/kernel-4.9
-#	patch -p1 < ~/buildLibrealsense2Xavier/patches/realsense-camera-formats_ubuntu-bionic-Xavier-4.9.140.patch 
-#	patch -p1 < ~/buildLibrealsense2Xavier/patches/realsense-metadata-ubuntu-bionic-Xavier-4.9.140.patch
-#	patch -p1 < ~/buildLibrealsense2Xavier/patches/realsense-hid-ubuntu-bionic-Xavier-4.9.140.patch
-#	patch -p1 < ~/realsense_src/librealsense-2.31.0/scripts/realsense-powerlinefrequency-control-fix.patch
-	# These are for the librealsense code, but don't actually seem to be used
-	#patch -p1 < ~/buildLibrealsense2Xavier/patches/model-views.patch
-	#patch -p1 < ~/buildLibrealsense2Xavier/patches/incomplete-frame.patch
-#	rm -rf ~/buildLibrealsense2Xavier
-
-	# turn on various config settings needed for USB tty, realsense, nvme, etc
-#	zcat /proc/config.gz > .config
-#    bash scripts/config --file .config \
-#        --set-str LOCALVERSION -tegra \
-#        --enable IIO_BUFFER \
-#        --enable IIO_KFIFO_BUF \
-#        --module IIO_TRIGGERED_BUFFER \
-#        --enable IIO_TRIGGER \
-#        --set-val IIO_CONSUMERS_PER_TRIGGER 2 \
-#        --module HID_SENSOR_IIO_COMMON \
-#        --module HID_SENSOR_IIO_TRIGGER \
-#        --module HID_SENSOR_HUB \
-#        --module HID_SENSOR_ACCEL_3D \
-#        --module HID_SENSOR_GYRO_3D \
-#        --module USB_ACM \
-#        --module CAN_GS_USB \
-#        --module JOYSTICK_XPAD \
-#        --enable CONFIG_BLK_DEV_NVME
-
-#	make -j6 clean
-#	make -j6 prepare
-#	make -j6 modules_prepare
-#    make -j6 Image zImage
-#    make -j6 modules
-#	sudo make -j6 modules_install
-#	make -j6 dtbs
-
-#	sudo depmod -a
-
-#    tar -C ~/jetson_kernel/kernel/kernel-4.9 -cjf ~/j120_hardware_dtb_l4t32-2-3-1.tbz2 \
-#        `find ~/jetson_kernel -name tegra186-quill-p3310-1000-a00-00-base.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra186-quill-p3310-1000-as-0888.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra186-quill-p3310-1000-c03-00-base.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra186-quill-p3310-1000-c03-00-dsi-hdmi-dp.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra186-quill-p3489-0888-a00-00-base.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra186-quill-p3489-1000-a00-00-ucm1.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra186-quill-p3489-1000-a00-00-ucm2.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra194-p2888-0001-p2822-0000.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name tegra194-p2888-0001-p2822-0000-maxn.dtb | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name Image | grep -v _ddot_` \
-#        `find ~/jetson_kernel -name zImage | grep -v _ddot_` 
-
-
-#    tar -cjf ~/l4t32-2-3-1-modules.tbz2 \
-#        `find /lib/modules/4.9.140-tegra/kernel -name hid-sensor-iio-common.ko` \
-#        `find /lib/modules/4.9.140-tegra/kernel -name hid-sensor-trigger.ko` \
-#        `find /lib/modules/4.9.140-tegra/kernel -name hid-sensor-hub.ko` \
-#        `find /lib/modules/4.9.140-tegra/kernel -name hid-sensor-accel-3d.ko` \
-#        `find /lib/modules/4.9.140-tegra/kernel -name hid-sensor-gyro-3d.ko` \
-#        `find /lib/modules/4.9.140-tegra/kernel -name cdc-acm.ko` \
-#        `find /lib/modules/4.9.140-tegra/kernel -name gs_usb.ko` \
-#        `find /lib/modules/4.9.140-tegra/kernel -name xpad.ko`
-
-	# make -j6 M=drivers/usb/class
-	# make -j6 M=drivers/usb/serial
-	# make -j6 M=drivers/net/can
-	# make -j6 M=net/can
-	# sudo mkdir -p /lib/modules/`uname -r`/kernel/drivers/usb/serial
-	# sudo cp drivers/usb/class/cp210x-acm.ko /lib/modules/`uname -r`/kernel/drivers/usb/serial/cp210x-acm.ko
-	# sudo mkdir -p /lib/modules/`uname -r`/kernel/drivers/usb/class
-	# sudo cp drivers/usb/serial/cdc-acm.ko /lib/modules/`uname -r`/kernel/drivers/usb/class/cdc-acm.ko
-	# sudo mkdir -p /lib/modules/`uname -r`/kernel/drivers/usb/class
-	# sudo cp drivers/net/can/usb/gs_usb.ko /lib/modules/`uname -r`/kernel/drivers/net/can/usb
-
-	# sudo mkdir -p /lib/modules/`uname -r`/kernel/drivers/joystick
-	# sudo cp xpad.ko /lib/modules/`uname -r`/kernel/drivers/joystick/xpad.ko
-	# sudo depmod -a
-#fi
-
-# Clean up Jetson
-#sudo rm -rf /home/nvidia/cudnn /home/nvidia/OpenCV /home/nvidia/TensorRT /home/nvidia/libvisionworkd*
-# Save ~400MB
-#sudo apt remove --purge -y thunderbird libreoffice-* unattended-upgrade
-
-# Install CTRE & navX libs
-mkdir -p /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include 
-mkdir -p /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ctre 
-cd /home/matthew-walstra 
-wget -e robots=off -U mozilla -r -np http://devsite.ctr-electronics.com/maven/release/com/ctre/phoenix/ -A "*5.16.0*,firmware-sim*zip" -R "md5,sha1,pom,jar,*windows*"
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include 
-find /home/matthew-walstra/devsite.ctr-electronics.com -name \*headers\*zip | xargs -n 1 unzip -o 
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ctre 
-find /home/matthew-walstra/devsite.ctr-electronics.com -name \*linux\*zip | xargs -n 1 unzip -o 
-rm -rf /home/matthew-walstra/devsite.ctr-electronics.com 
-cd /home/matthew-walstra 
-wget -e robots=off -U mozilla -r -np http://devsite.ctr-electronics.com/maven/release/com/ctre/phoenix/ -A "*5.18.3*,firmware-sim*zip" -R "md5,sha1,pom,jar,*windows*"
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include 
-find /home/matthew-walstra/devsite.ctr-electronics.com -name \*headers\*zip | xargs -n 1 unzip -o 
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ctre 
-find /home/matthew-walstra/devsite.ctr-electronics.com -name \*linux\*zip | xargs -n 1 unzip -o 
-rm -rf /home/matthew-walstra/devsite.ctr-electronics.com 
-
-cd /home/matthew-walstra 
-wget http://www.kauailabs.com/maven2/com/kauailabs/navx/frc/navx-cpp/3.1.400/navx-cpp-3.1.400-headers.zip 
-mkdir -p /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/navx 
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/navx 
-unzip -o /home/matthew-walstra/navx-cpp-3.1.400-headers.zip 
-rm /home/matthew-walstra/navx-cpp-3.1.400-headers.zip 
-cd /home/matthew-walstra 
-wget http://www.kauailabs.com/maven2/com/kauailabs/navx/frc/navx-cpp/3.1.400/navx-cpp-3.1.400-linuxathena.zip 
-mkdir -p /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/navx 
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/navx 
-unzip -o /home/matthew-walstra/navx-cpp-3.1.400-linuxathena.zip 
-rm /home/matthew-walstra/navx-cpp-3.1.400-linuxathena.zip 
-cd /home/matthew-walstra 
-wget http://www.kauailabs.com/maven2/com/kauailabs/navx/frc/navx-cpp/3.1.400/navx-cpp-3.1.400-linuxathenastatic.zip 
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/navx 
-unzip -o /home/matthew-walstra/navx-cpp-3.1.400-linuxathenastatic.zip 
-rm /home/matthew-walstra/navx-cpp-3.1.400-linuxathenastatic.zip 
-
-# Install wpilib headers by copying them from the local maven dir
-# TODO - need to update to acutal 2020 filenames once they are released
-cd /home/matthew-walstra 
-wget https://github.com/wpilibsuite/allwpilib/releases/download/v2020.3.2/WPILib_Linux-2019.3.2.tar.gz 
-mkdir -p /home/matthew-walstra/wpilib/2020 
-cd /home/matthew-walstra/wpilib/2020 
-tar -xzf /home/matthew-walstra/WPILib_Linux-2020.3.2.tar.gz 
-rm /home/matthew-walstra/WPILib_Linux-2020.3.2.tar.gz 
-cd /home/matthew-walstra/wpilib/2020/tools 
-python3 ToolsUpdater.py 
-mkdir -p /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/wpilib 
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/wpilib 
-find ../../../.. -name \*athena\*zip | xargs -n1 unzip -o 
-mkdir -p /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/wpilib 
-cd /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/wpilib 
-find ../../../.. -name \*headers\*zip | xargs -n1 unzip -o 
-rm -rf /home/matthew-walstra/wpilib/2020/maven /home/matthew-walstra/wpilib/2020/jdk
-sed -i -e 's/   || defined(__thumb__) \\/   || defined(__thumb__) \\\n   || defined(__aarch64__) \\/' /home/matthew-walstra/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/wpilib/FRC_FPGA_ChipObject/fpgainterfacecapi/NiFpga.h
-
-# Set up prereqs for deploy script
-#mv ~/2019Offseason ~/2019Offseason.orig
-#ln -s ~/2019Offseason.orig ~/2019Offseason
-#mkdir -p ~/2019Offseason.prod/zebROS_ws
-#mkdir -p ~/2019Offseason.dev/zebROS_ws
-
-#sudo mkdir -p /usr/local/zed/settings
-#sudo chmod 755 /usr/local/zed/settings
-#sudo cp ~/2019Offseason/calibration_files/*.conf /usr/local/zed/settings
-#sudo chmod 644 /usr/local/zed/settings/*
-
-#cp ~/2019Offseason/.vimrc ~/2019Offseason/.gvimrc ~
-#sudo cp ~/2019Offseason/kjaget.vim /usr/share/vim/vim80/colors
-
-#git config --global user.email "progammers@team900.org"
-#git config --global user.name "Team900 Jetson TX2"
-
 # Set up Gold linker - speed up libPCL links
 #sudo update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 20
 #sudo update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.bfd" 10
 
 #echo | sudo update-alternatives --config ld
 
-#echo "source /home/matthew-walstra/2019Offseason/zebROS_ws/command_aliases.sh" >> /home/matthew-walstra/.bashrc
+mkdir -p ~/Swerve_ws/CTRE/include
+mkdir -p ~/Swerve_ws/CTRE/lib/ctre
+
+cd ~/Swerve_ws/CTRE
+wget -e robots=off -U mozilla -r -np http://devsite.ctr-electronics.com/maven/release/com/ctre/phoenix/ -A "*5.18.3*,firmware-sim*zip" -R "md5,sha1,pom,jar,*windows*"
+cd ~/Swerve_ws/CTRE/include
+find ~/Swerve_ws/CTRE/devsite.ctr-electronics.com -name \*headers\*zip | xargs -n 1 unzip -o 
+cd ~/Swerve_ws/CTRE/lib/ctre
+find ~/Swerve_ws/CTRE/devsite.ctr-electronics.com -name \*linux\*zip | xargs -n 1 unzip -o 
+rm -rf ~/Swerve_ws/CTRE/devsite.ctr-electronics.com
+cd ~
+
+
 
